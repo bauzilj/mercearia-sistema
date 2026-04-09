475a371a077d5953a89d59d1d6d9aca87ef54139
Jean Joseph Bauzil <jeanjosephbauzil@gmail.com>
2026-04-09 17:42:34 -0300

    adding cliente JS

--- assets/js/cliente.js
+++ assets/js/cliente.js
@@ -27,18 +27,70 @@ function criarLinha(cliente) {
         <td>${cliente.id}</td>
         <td>${cliente.nome}</td>
         <td>${cliente.email}</td>
-        <td><span class="badge neutral">${cliente.status || "Sem status"}</span></td>
         <td>
           <div class="table-actions">
-            <button nclick="editar(this)" class="btn-action edit">Editar</button>
+            <button class="btn-action edit">Editar</button>
             <button class="btn-action delete">Excluir</button>
           </div>
         </td>
     `;
 
+    // Pegando os botões
+    const btnEditar = tr.querySelector(".edit");
+    const btnExcluir = tr.querySelector(".delete");
+
+    // Evento de editar
+    btnEditar.addEventListener("click", () => {
+        editarCliente(cliente);
+    });
+
+    // Evento de excluir
+    btnExcluir.addEventListener("click", () => {
+        excluirCliente(cliente.id);
+    });
+
     tbodyClientes.appendChild(tr);
 }
 
-carregarClientes();
+carregarClientes()
 
+function excluirCliente(id) {
+    if (!confirm("Tem certeza que deseja excluir este cliente?")) return;
+
+    fetch(`${API_CLIENTES_BASE_URL}/${id}`, {
+        method: "DELETE"
+    })
+        .then(() => {
+            alert("Cliente excluído com sucesso!");
+            carregarClientes(); 
+        })
+        .catch(error => {
+            console.error("Erro ao excluir:", error);
+        });
+}
+
+function editarCliente(cliente) {
+    const novoNome = prompt("Novo nome:", cliente.nome);
+    const novoEmail = prompt("Novo email:", cliente.email);
+
+    if (!novoNome || !novoEmail) return;
+
+    fetch(`${API_CLIENTES_BASE_URL}/${cliente.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: novoNome,
+            email: novoEmail
+        })
+    })
+        .then(() => {
+            alert("Cliente atualizado!");
+            carregarClientes();
+        })
+        .catch(error => {
+            console.error("Erro ao editar:", error);
+        });
+}
 
